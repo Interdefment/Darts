ecc10e3418ed8785e6752e55a09d71f06c02991e
Interdefment <Interdefment@users.noreply.github.com>
2018-07-22 19:57:24 +0300

    Стартовое меню без дизайна

--- app/js/common.js
+++ app/js/common.js
@@ -24,22 +24,106 @@ var darts = new Vue({
 			{	value: 5,},
 		],
 		players: [
-			{ id: 1, name: 'Саша', points: 0, totalPoints: 0 },
-			{ id: 2, name: 'Дима', points: 0, totalPoints: 0 },
-			{ id: 3, name: 'Женя', points: 0, totalPoints: 0 },
+			{ id: 1, name: 'Саша', points: 0, totalPoints: 0, avatar: 2, picked: false },
+			{ id: 2, name: 'Дима', points: 0, totalPoints: 0, avatar: 19, picked: false },
+			{ id: 3, name: 'Женя', points: 0, totalPoints: 0, avatar: 15, picked: false },
+			{ id: 4, name: 'Паша', points: 0, totalPoints: 0, avatar: 28, picked: false }
 		],
 		currentPlayer: 0,
 		clickCounter: 0,
+		overlayClosed: false,
+		newName: '',
+		avatarChoosing: false,
+		avatarPicked: 0,
+	},
+	computed: {
+		pickedPlayers: function () {
+			return this.players.filter(function (player) {
+				return player.picked
+			})
+		},
+		freePlayers: function () {
+			return this.players.filter(function (player) {
+				return !player.picked
+			})
+		},
 	},
 	methods: {
-		shoot: function SH (points) {
-			console.log(points)
+		updateScroll: function updateScroll () {
+				setTimeout(function() {
+					$(".nano").nanoScroller();
+				}, 500);
+		},
+		shoot: function shoot (points) {
 			this.players[this.currentPlayer].points += points;
 			if (++this.clickCounter == 3) {
 				this.clickCounter = 0;
-				this.currentPlayer = this.currentPlayer == 2 ? 0 : this.currentPlayer + 1;
-
+				this.currentPlayer = 
+					this.currentPlayer == this.pickedPlayers.length - 1 ? 0
+					: this.currentPlayer + 1;
 			}
-		}
+		},
+		close: function close () {
+			this.overlayClosed = true;
+		},
+		closeAvatarLib: function closeAvatarLib () {
+			this.avatarChoosing = false;
+		},
+		openModal: function openModal () {
+			this.overlayClosed = false;
+		},
+		addPlayer: function addPlayer (e) {
+			if (!avatarPicked || this.newName == '')
+				return false;
+			const newPlayer = { 
+				id: this.players.length + 1,
+				name: this.newName,
+				points: 0,
+				totalPoints: 0,
+				avatar: avatarPicked,
+				picked: true
+			}
+			this.players.push(newPlayer);
+			this.updateScroll();
+			this.newName = '';
+			$('.new-avatar').attr('src','img/avatars/noname.png');
+		},
+		checkAvatar: function checkAvatar (number) {
+			for (let i = 0; i < this.players.length; i++) {
+				if (this.players[i].avatar == number) {
+				 return true;
+				}
+			}
+			return false;
+		},
+		openAvatarLib: function chooseAva () {
+			this.avatarChoosing = true;
+			this.updateScroll();
+		},
+		pickAvatar: function pickAvatar (number) {
+			avatarPicked = number;
+			$('.new-avatar').attr('src','img/avatars/' +  number + '.png');
+			this.closeAvatarLib();
+		},
+		startPlay: function startPlay () {
+			if (this.pickedPlayers.length < 2)
+				return false;
+			this.close();
+		},
+	},
+	mounted: function () {
+		for (let i = 1; i < 96; i++) {
+			if (this.checkAvatar(i)) {
+				continue;
+			}
+			let img = '<img src="img/avatars/' + i + '.png" alt="' + i + '" class="avatar" data-number="' + i + '">'
+			$('.avatar-lib .nano-content').append(img);			
+		};
 	}
 })
+$('.avatar-lib .avatar').on('click', function(e) {
+	darts.pickAvatar($(this).attr('data-number'))
+});
+$(document).ready(function() {
+	$(".nano").nanoScroller({falsh:true });
+});
