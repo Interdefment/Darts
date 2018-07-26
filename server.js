2c4556d9d22443f93cb74c31a7e98b89b81acaa4
grigoryevalan <39834034+grigoryevalan@users.noreply.github.com>
2018-07-26 10:14:13 +0300

    correct server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.static(__dirname));
 
 app.get('/', function(req, res) {
-	res.sendFile(__dirname + '/SeaBattle.html');
+	res.sendFile(__dirname + 'app/index.html');
 });
 // app.get('/favicon.ico', function(req, res) {
 // 	res.sendFile(__dirname + '/favicon.ico');
