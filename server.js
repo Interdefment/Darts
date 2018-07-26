86e87024244d882d9b706deeb39bb160f757ffe0
grigoryevalan <39834034+grigoryevalan@users.noreply.github.com>
2018-07-26 10:24:38 +0300

    correct server.js 3

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static(__dirname));
+app.use(express.static(__dirname + '/app'));
 
 app.get('/', function(req, res) {
 	res.sendFile(__dirname + '/app/index.html');
